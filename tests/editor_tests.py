a01ac3b46e509c3186a1691cb4434ecaf3201ea6
Karl Ramm <kcr@1ts.org>
2014-04-03 00:34:06 -0400

    style tweaking of editor fuzz test

--- tests/editor_tests.py
+++ tests/editor_tests.py
@@ -136,27 +136,38 @@ class TestEditor(unittest.TestCase):
         self.assertEqual(
             [(int(m), l) for (m, l) in e.view(0, 'forward')],
             [(0, [((), u'abc'), (('cursor', 'visible'), u'')])])
-    def testfuzz(self, iterations=10000, max_len=74, max_op_len=10, show_delay=0.01):
-        """For many <iterations> randomly either insert or delete
-           up to <max_op_len> chars or just move the gap around.
+    def testfuzz(
+        self,
+        iterations=10000,
+        max_len=74,
+        max_op_len=10,
+        show_delay=0.01,
+        ):
+        """
+        For many <iterations> randomly either insert or delete up to
+        <max_op_len> chars or just move the gap around.
 
-           Make sure the entire thing is never more than max_len chars long.
-           
-           Make a parallel array and confirm after each operation that the gap
-           buffer's contents are the same as the array's.
-           
-           If show_delay is > 0, then the gap buffer will be shown each iteration.  If it's
-           0 then nothign will display during iteration, but the operations will be dumped
-           after all iterations.
-           """
-        contents = u"abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        Make sure the entire thing is never more than max_len chars long.
+
+        Make a parallel array and confirm after each operation that
+        the gap buffer's contents are the same as the array's.
+
+        If show_delay is > 0, then the gap buffer will be shown each
+        iteration.  If it's 0 then nothign will display during
+        iteration, but the operations will be dumped after all
+        iterations.
+        """
+
+        contents = (
+            u'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
         g = snipe.editor.Editor(None)
-        g.set_content(u"")
-        a = array.array("u")
+        g.set_content(u'')
+        a = array.array('u')
         for n in range(iterations):
             pos = g.size and random.randint(0, g.size-1)
-            # A 1 in 3 chance to delete, unless we're at max length, then delete regardless
+            # A 1 in 3 chance to delete, unless we're at max length,
+            # then delete regardless
             if not random.randint(0, 2) or g.size >= max_len:
                 howmany = min(g.size - pos, random.randint(1, max_op_len))
                 g.cursor.point = pos
@@ -167,13 +178,15 @@ class TestEditor(unittest.TestCase):
                 g.cursor.point = pos
                 # A 1 in 2 chance to insert instead of just moving th gap
                 if not random.randint(0, 1):
-                    howmany = random.randint(1, max(max_op_len, max_len - g.size))
+                    howmany = random.randint(
+                        1, max(max_op_len, max_len - g.size))
                     char = random.choice(contents)
                     g.insert(char * howmany)
                     for i in range(howmany):
-                        a.insert(pos, char) 
-            assert a.tounicode() == g.text
+                        a.insert(pos, char)
+            self.assertEqual(a.tounicode(), g.text)
             print g.text
-        
+
+
 if __name__ == '__main__':
     unittest.main()
