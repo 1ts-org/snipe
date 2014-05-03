e53c1bd8485310d97ece957270d498654c199b86
Karl Ramm <kcr@1ts.org>
2014-05-03 12:54:30 -0400

    simple snipe._rooster.Rooster test program

---
+++ rooster.py
@@ -0,0 +1,17 @@
+import asyncio
+import pprint
+import logging
+
+import snipe._rooster
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    loop = asyncio.get_event_loop()
+    r = snipe._rooster.Rooster(
+        'https://ordinator.1ts.org', 'daemon@ordinator.1ts.org')
+    loop.run_until_complete(
+        r.newmessages(
+            asyncio.coroutine(lambda m: pprint.pprint(m))))
+
+if __name__ == '__main__':
+    main()
