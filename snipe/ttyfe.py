1ts-org/snipe

commit 10e508d3df47a3bc94f0e4ffdbf6946d7e629376

fix reframe fencepost

snipe/ttyfe.py
@@ -365,10 +365,10 @@ class TTYRenderer:
             if screenlines <= 0:
                 break
             self.log.debug('reframe, loop bottom, mark=%x, /offset=%d', id(mark), max(0, -screenlines))
-        self.head = Location(self, mark, max(0, (- screenlines) - 1))
-        self.log.debug('reframe, post-loop,   mark=%x, /offset=%d: %s', id(mark), max(0, -screenlines), repr(self.head))
+        self.head = Location(self, mark, max(0, -screenlines))
 
-        self.log.debug('reframe, screenlines=%d, head=%s', screenlines, repr(self.head))
+        self.log.debug('reframe, post-loop,   mark=%x, /offset=%d', id(mark), max(0, -screenlines))
+        self.log.debug('reframe, post-loop, screenlines=%d, head=%s', screenlines, repr(self.head))
 
     def chunksize(self, chunk):
         lines = 0

commit eef6edb1c2e974d3422e1f73d36e335b7eae8c49

10e508d broke one-line windows with the cursor not at column zero
The current model doesn't account for cursors not at column zero very well,
but at least this _almost_ seems to behave as expected.

snipe/ttyfe.py
@@ -354,18 +354,28 @@ class TTYRenderer:
         self.head = Location(self, cursor)
         self.log.debug('reframe, initial,     mark=%x: %s', id(cursor), repr(self.head))
 
-        for mark, chunk in self.window.view(self.window.cursor, 'backward'):
-            # this should only drop stuff off the first chunk...
-            chunk = itertools.takewhile(
-                lambda x: 'visible' not in x[0],
-                chunk)
-            chunklines = self.chunksize(chunk)
-            self.log.debug('reframe, screenlines=%d, len(chunklines)=%s', screenlines, chunklines)
-            screenlines -= chunklines
-            if screenlines <= 0:
-                break
-            self.log.debug('reframe, loop bottom, mark=%x, /offset=%d', id(mark), max(0, -screenlines))
-        self.head = Location(self, mark, max(0, -screenlines))
+        view = self.window.view(self.window.cursor, 'backward')
+
+        mark, chunk = next(view)
+        chunk = itertools.takewhile(
+            lambda x: 'visible' not in x[0],
+            chunk)
+        chunklines = self.chunksize(chunk)
+        self.log.debug('reframe cursor chunk, screenlines=%d, len(chunklines)=%s', screenlines, chunklines)
+        screenlines -= chunklines
+        self.log.debug('reframe cursor chunk, loop bottom, mark=%x, /offset=%d', id(mark), max(0, -screenlines))
+
+        if screenlines <= 0:
+            self.head = Location(self, mark, max(0, -screenlines - 1))
+        else:
+            for mark, chunk in view:
+                chunklines = self.chunksize(chunk)
+                self.log.debug('reframe, screenlines=%d, len(chunklines)=%s', screenlines, chunklines)
+                screenlines -= chunklines
+                if screenlines <= 0:
+                    break
+                self.log.debug('reframe, loop bottom, mark=%x, /offset=%d', id(mark), max(0, -screenlines))
+            self.head = Location(self, mark, max(0, -screenlines))
 
         self.log.debug('reframe, post-loop,   mark=%x, /offset=%d', id(mark), max(0, -screenlines))
         self.log.debug('reframe, post-loop, screenlines=%d, head=%s', screenlines, repr(self.head))