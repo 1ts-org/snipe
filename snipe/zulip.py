cc7134a090cd4234ef59f0efc83c280ab37912c6
Karl Ramm <kcr@1ts.org>
2016-11-20 23:11:55 -0500

    proper message rendering &c

--- snipe/zulip.py
+++ snipe/zulip.py
@@ -40,6 +40,9 @@ import aiohttp
 import asyncio
 import netrc
 import os
+import pprint
+import textwrap
+import time
 import urllib.parse
 
 from . import messages
@@ -48,6 +51,9 @@ from . import util
 
 class Zulip(messages.SnipeBackend, util.HTTP_JSONmixin):
     name = 'zulip'
+    loglevel = util.Level(
+        'log.zulip', 'Zulip',
+        doc='loglevel for zulip backend')
 
     def __init__(self, context, url='https://chat.zulip.org', **kw):
         super().__init__(context, **kw)
@@ -73,14 +79,15 @@ class Zulip(messages.SnipeBackend, util.HTTP_JSONmixin):
         self.user = authdata[0]
         self.token = authdata[2]
 
-        params = None
+        self.params = None
         while True:
             import pprint
-            if params is None:
+            if self.params is None:
                 self.log.debug('registering')
                 params = yield from self._post('register')
 
                 # TODO check for an error, backoff, etc.
+                self.params = params
 
                 queue_id = params['queue_id']
                 last_event_id = params['last_event_id']
@@ -91,12 +98,24 @@ class Zulip(messages.SnipeBackend, util.HTTP_JSONmixin):
             # TODO check for error and maybe invalidate params?
 
             msgs = [
-                messages.SnipeMessage(self, pprint.pformat(event) + '\n')
-                for event in result['events']]
+                ZulipMessage(self, event)
+                for event in result['events']
+                if event.get('type') == 'message']
             last_event_id = max(*[last_event_id] + [
                 int(event['id']) for event in result['events']])
-            self.messages.extend(msgs)
-            self.redisplay(msgs[0], msgs[-1])
+            if msgs:
+                self.messages.extend(msgs)
+                # make sure that the message list remains
+                # monitonically increasing by comparing the new
+                # messages (and the last old message) pairwise.
+                for a, b in zip(
+                        self.messages[-len(msgs) - 1:][:-1],
+                        self.messages[-len(msgs) - 1:][1:]):
+                    if b.time <= a.time:
+                        self.log.debug('before %f, %f', a.time, b.time)
+                        b.time = a.time + .0001
+                        self.log.debug('after %f, %f', a.time, b.time)
+                self.redisplay(msgs[0], msgs[-1])
 
         self.log.debug('connect ends')
 
@@ -118,3 +137,84 @@ class Zulip(messages.SnipeBackend, util.HTTP_JSONmixin):
             params=kw,
             )
         return result
+
+
+class ZulipMessage(messages.SnipeMessage):
+
+    def __init__(self, backend, event):
+        super().__init__(
+            backend,
+            pprint.pformat(event),
+            float(event['message'].get('timestamp', time.time())),
+            )
+        self.data = event['message']
+
+        self._sender = ZulipAddress(backend, event.get('sender_email', '?'))
+        if self.data.get('type') == 'stream':
+            self.stream = str(self.data['display_recipient'])
+        elif self.data.get('type') == 'private':
+            self.personal = True
+        else:
+            backend.log.debug('weird message: %s', pprint.pformat(event))
+            self.noise = True
+
+    def display(self, decoration):
+        tags = set(self.decotags(decoration))
+
+        if self.data['type'] == 'private':
+            title = ', '.join([
+                x.get('short_name', x.get('email', str(x)))
+                for x in self.data['display_recipient']])
+        else:
+            # we expect it's a string already, buuuut....
+            title = str(self.data['display_recipient'])
+
+        subject = self.data.get('subject')
+        if subject:
+            subject = ' ' + subject
+        else:
+            subject = ''
+
+        name = self.data.get('sender_full_name')
+        if name:
+            name = ' ' + name
+        else:
+            name = ''
+
+        timestamp = time.strftime(
+            ' %H:%M:%S\n', time.localtime(self.data['timestamp']))
+
+        body = self.data.get('content')
+        body = body.replace('\r\n', '\n')  # conform to local custom
+
+        # make markdown with long lines readable
+        # (really, replace this with a markdown renderer that does literal text
+        # & _underlining_ & *bold* &c correctly)
+        body = '\n\n'.join(
+            '\n'.join(textwrap.wrap(s, 72)) for s in body.split('\n\n'))
+        if body[-1] != '\n':
+            body += '\n'
+
+        return [(tuple(x), y) for (x, y) in
+            [
+            (tags | {'bold'}, title + '>'),
+            (tags, subject + ' <'),
+            (tags | {'bold'}, self.data.get('sender_email', '?')),
+            (tags, '>' + name),
+            (tags | {'right'}, timestamp),
+            (tags, body)
+            ]]
+
+
+class ZulipAddress(messages.SnipeAddress):
+
+    def __init__(self, backend, text):
+        self.backend = backend
+        self.text = text
+        super().__init__(backend, [text])
+
+    def __str__(self):
+        return self.backend.name + '; ' + self.short()
+
+    def short(self):
+        return self.text
