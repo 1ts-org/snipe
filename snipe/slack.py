b1c7422c4d011b15cdd3c2e373f51d823463ea75
Karl Ramm <kcr@1ts.org>
2016-11-24 15:44:54 -0500

    work around an issue with with aiohttp >= 1.1
    and its use of yarl.

--- snipe/slack.py
+++ snipe/slack.py
@@ -47,6 +47,7 @@ import contextlib
 import itertools
 
 import asyncio
+import aiohttp
 
 from . import messages
 from . import util
@@ -164,6 +165,21 @@ class Slack(messages.SnipeBackend, util.HTTP_JSONmixin):
                 [(x['id'], SlackDest(self, t, x)) for x in self.data[t + 's']]
                 for t in ['user', 'bot', 'im', 'group', 'channel']
                 ), []))
+
+            # Slack's websocket servers want a literal '=' in the request and
+            # not a %3D.  I don't know why, and don't really care, but this is
+            # how I trick yarl & http into obliging.
+            yarl = None
+            try:
+                import yarl
+            except ImportError:
+                pass
+            aiohttp_vers = float('.'.join(aiohttp.__version__.split('.')[:2]))
+            if yarl is not None and aiohttp_vers >= 1.1:
+                url = yarl.URL(url)
+                url._val = urllib.parse.SplitResult(
+                    url.scheme, url.netloc, url.path.replace('%3D', '='),
+                    url.query, url.fragment)
 
             self.log.debug('websocket url is %s', url)
 
