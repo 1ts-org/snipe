2628598718e3b0dfb4e18a7bb8ad515648bc4e9e
Karl Ramm <kcr@1ts.org>
2017-08-05 22:12:14 -0400

    flag as python 3 only another way

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ setup(
         'Environment :: Console :: Curses',
         'Framework :: AsyncIO',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only',
         ],
     long_description="""
 snipe is a text-oriented (currently curses-based) "instant" messaging
 client intended for services with persistence, such as Zulip
 (https://zulip.org/), also IRCCloud (https://www.irccloud.com) and
 roost (https://github.com/roost-im).""",
-    version='0.dev0',
+    version='0.dev1',
     packages=find_packages(exclude=['tests']),
     python_requires='>=3.3',
     entry_points={'console_scripts': [
