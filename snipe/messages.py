6139fdb686da2e870392eaabac5fb6f701ffd736
Karl Ramm <kcr@1ts.org>
2018-12-19 02:23:32 -0500

    improve backend query error

--- snipe/messages.py
+++ snipe/messages.py
@@ -711,10 +711,13 @@ class AggregatorBackend(SnipeBackend):
     async def send(self, paramstr, msg):
         params = [s.strip() for s in paramstr.split(';', 1)]
         backends = [b for b in self if b.name.startswith(params[0])]
+        if False and not params[0]:
+            raise util.SnipeException('no backend in query string')
         if len(backends) != 1:
+            backend_str = ' '.join(str(backend) for backend in backends)
             raise util.SnipeException(
-                'backend query string %s results in %s' % (
-                    params[0], backends))
+                f'ambiguous backend {params[0]!r}:'
+                f' possibly one of f{backend_str}')
         await backends[0].send(''.join(params[1:]), msg)
 
     def backfill(self, filter, target=None):
