6bf97c6d07653b991c0c3ff5cbe28a761ecf2e88
Karl Ramm <kcr@1ts.org>
2019-03-21 22:27:12 -0400

    tests for interactive.py

--- tests/interactive_tests.py
+++ tests/interactive_tests.py
@@ -32,11 +32,118 @@
 Unit tests for interactive function infrastructure
 '''
 
+import inspect
 import unittest
+
+from typing import TYPE_CHECKING
 
 import snipe.interactive as interactive  # noqa: F401
 
 
 class TestInteractive(unittest.TestCase):
-    def test_null(self):
-        pass
+    def test__keyword(self):
+        f = interactive._keyword('foo')
+
+        self.assertTrue(inspect.isfunction(f))
+
+        self.assertIsNone(f())
+        self.assertEqual('bar', f(foo='bar'))
+
+    def test_integer_argument(self):
+        if TYPE_CHECKING:
+            return
+        self.assertEqual(
+            5, interactive.integer_argument(argument=5))
+        self.assertEqual(
+            -1, interactive.integer_argument(argument='-'))
+        self.assertEqual(
+            4,  interactive.integer_argument(argument=[True]))
+        self.assertEqual(
+            3, interactive.positive_integer_argument(
+                argument=-3))
+        self.assertIsNone(interactive.positive_integer_argument())
+
+    def test_isinteractive(self):
+        self.assertTrue(interactive.isinteractive())
+
+    def test_call(self):
+        def callable(i: interactive.integer_argument):
+            return 5
+
+        self.assertEqual(5, interactive.call(callable))
+
+        def callable(i: interactive.integer_argument=4):
+            return i
+
+        self.assertEqual(4, interactive.call(callable))
+
+        def callable(i):
+            pass
+
+        with self.assertRaises(Exception):
+            interactive.call(callable)
+
+
+class TestUnCompleter(unittest.TestCase):
+    def test(self):
+        u = interactive.UnCompleter()
+        self.assertEqual([], u.candidates)
+        self.assertFalse(u.live)
+
+        self.assertEqual([], u.matches())
+        self.assertIsNone(u.roll(5))
+        self.assertIsNone(u.roll_to('foo'))
+        self.assertFalse(u.check(None, None))
+        self.assertEqual((None, None), u.expand('value'))
+
+
+class TestCompleter(unittest.TestCase):
+    def test(self):
+        c = interactive.Completer(['aaa', 'bab', 'aab'])
+        self.assertEqual(
+            [(0, 'aaa', 'aaa'), (1, 'aab', 'aab')], c.matches('aa'))
+        c.roll(1)
+        self.assertEqual(
+            [(0, 'aab', 'aab'), (2, 'aaa', 'aaa')], c.matches('aa'))
+        c.roll_to('aaa')
+        self.assertEqual(
+            [(0, 'aaa', 'aaa'), (1, 'aab', 'aab')], c.matches('aa'))
+        self.assertEqual('aaa', c.expand('a'))
+        self.assertEqual('ccc', c.expand('ccc'))
+
+
+class TestFileCompleter(unittest.TestCase):
+    def test(self):
+        f = interactive.FileCompleter()
+        self.assertEqual(1, len(f.matches('interactive')))
+        self.assertEqual(
+            ('interactive_tests.py', 'interactive_tests.py'),
+            f.matches('interactive')[0][1:])
+        self.assertEqual('interactive_tests.py', f.expand('interactive'))
+        f.directory = '../tests'
+        self.assertEqual(
+            ('interactive_tests.py', 'interactive_tests.py'),
+            f.matches('interactive')[0][1:])
+        self.assertEqual(
+            '../tests/interactive_tests.py',
+            f.expand('../tests/interactive'))
+
+
+class TestDestCompleter(unittest.TestCase):
+    def test(self):
+        roost = unittest.mock.Mock()
+        roost.name = 'roost'
+        irccloud = unittest.mock.Mock()
+        irccloud.name = 'irccloud'
+        context = unittest.mock.Mock()
+        context.backends = [roost, irccloud]
+        d = interactive.DestCompleter(
+            ['roost; foo', 'roost; bar', 'irccloud; baz'], context)
+        self.assertEqual(['roost; bar'], [x[1] for x in d.matches('bar')])
+        self.assertEqual(['roost; bar'], [x[1] for x in d.matches('r;bar')])
+        self.assertEqual(
+            ['irccloud; baz'], [x[1] for x in d.matches('i;baz')])
+        self.assertEqual(
+            ['irccloud; baz'], [x[1] for x in d.matches('i;')])
+        self.assertEqual(
+            ['irccloud; baz'], [x[1] for x in d.matches(';baz')])
